c021a7260dc2e552a6b8818a2344a3598a6ec800
Johan Myrsmeden <johan@myrsmeden.se>
2019-05-25 10:34:43 +0200

    More empty state

--- njin/src/njin/core.clj
+++ njin/src/njin/core.clj
@@ -6,8 +6,20 @@
   []
   {:test (fn []
            (is (= (keys (create-empty-state))
-                  [:enemies])))}
-  {:enemies [{:name "Filip"}]})
+                  [:enemies :defenders :lives :wave :gold])))}
+  {:enemies [{:bounty 7
+              :health 60
+              :id 1
+              :position {:x 0
+                         :y 0}
+              :direction {:x 10
+                          :y 0}}]
+   :defenders [:position {:x 500
+                          :y 100
+                          :range 50}]
+   :lives 3
+   :wave 0
+   :gold 100})
 
 (defn main
   "Main function"
