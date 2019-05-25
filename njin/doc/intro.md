ca38faf662f1485a036036a8d34ebeccb090e05d
Filip Johansson <filip@aiwizo.com>
2019-05-25 09:30:14 +0200

    Initial commit

---
+++ njin/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to njin
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ njin/test/njin/core_test.clj
@@ -0,0 +1,7 @@
+(ns njin.core-test
+  (:require [clojure.test :refer :all]
+            [njin.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
