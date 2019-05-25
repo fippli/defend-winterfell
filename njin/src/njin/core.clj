02e5bd6ce85d6f25897cdd5db4475fa44b24b70f
Johan Myrsmeden <johan@myrsmeden.se>
2019-05-25 10:13:22 +0200

    Adding create-empty-state

--- njin/src/njin/core.clj
+++ njin/src/njin/core.clj
@@ -1,4 +1,13 @@
-(ns njin.core)
+(ns njin.core
+  (:require [clojure.test :refer [function? is]]))
+
+(defn create-empty-state
+  "Creates an empty state"
+  []
+  {:test (fn []
+           (is (= (keys (create-empty-state))
+                  [:enemies])))}
+  {:enemies [{:name "Filip"}]})
 
 (defn main
   "Main function"
